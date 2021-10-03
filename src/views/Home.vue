e54ee9e31aa2d9c696f188ad4485d9afca58d7e7
Massimiliano <massimiliano.porzio@gmail.com>
2021-10-03 21:16:01 +0200

    vue composition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="home">
-   home
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">Stop Watching</button>
   </div>
 </template>
 
 <script setup>
+import {computed, reactive, ref, watch, watchEffect} from "vue";
 const name= 'Home'
+const search = ref('')
+const stopWatch = watch(search,()=>{
+  console.log("WATCH")
+})
 
+const handleClick = ()=>{
+  console.log('CLICK')
+  stopWatch()
+  stopWatchEffect()
+}
+
+const stopWatchEffect=watchEffect(()=>{
+  console.log('watchEffect',search.value)
+})
+const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+const matchingNames = computed(() => {
+  // return ['a', 'b', 'c']
+  return names.value.filter(name => name.includes(search.value))
+})
 </script>
