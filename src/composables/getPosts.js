0d23fc0e9b3b9195b3e0962f6d6ee6201e03f714
Massimiliano <massimiliano.porzio@gmail.com>
2021-10-08 23:31:37 +0200

    get all docs

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,19 +1,35 @@
 import {ref} from "vue";
-
+import {projectFirestore} from "@/firebase/config";
+import { collection, getDocs } from "firebase/firestore";
 const getPosts = () => {
 
     const posts = ref([])
     const error = ref(null)
     const load = async () => {
         try {
-            //simulate delay
-            await new Promise(resolve => {setTimeout(resolve,200)})
-            let data = await fetch("http://localhost:3000/posts")
-            if (!data.ok) {
-                //problem
-                throw Error('no data available')
-            }
-            posts.value = await data.json()
+            // //simulate delay
+            // await new Promise(resolve => {setTimeout(resolve,200)})
+            // let data = await fetch("http://localhost:3000/posts")
+            // if (!data.ok) {
+            //     //problem
+            //     throw Error('no data available')
+            // }
+            // posts.value = await data.json()
+            const ref = collection(projectFirestore,"posts") //reference to the collection I want to fetch
+            const querySnapshot = await getDocs(ref) // async
+            // console.log(querySnapshot.docs)
+
+            posts.value = querySnapshot.docs.map(doc=>{
+                // console.log(doc.data())
+                return {...doc.data(),id: doc.id}
+            })
+            // querySnapshot.forEach((doc) => {
+            //     // doc.data() is never undefined for query doc snapshots
+            //     console.log(doc.id, " => ", doc.data());
+            // });
+
+            // })
+
         } catch (err) {
             error.value = err.message
             console.log(err.message)
